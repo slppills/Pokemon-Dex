c11993f91c13cad4e9eb597eae5d563dfe4b4282
slppills <sunchaehoon2@naver.com>
2024-08-24 16:20:14 +0900

    update: Router

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Dex from "../pages/Dex";
+import Layout from "./Layout";
+import Dashboard from "../components/Dashboard";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dex" element={<Dex />} />
+          <Route path="/pokemon-detail" element={<Dashboard />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
