b6d79ba9dd889384c14836d73e0427a6ea42f539
slppills <sunchaehoon2@naver.com>
2024-08-26 00:15:14 +0900

    update: redux Provider

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,18 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Dex from "../pages/Dex/Dex";
 import Layout from "./Layout";
-import Dashboard from "../components/Dashboard/Dashboard";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import { Provider } from "react-redux";
+import store from "../redux/config/configStore";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dex" element={<Dex />} />
-          <Route path="/pokemon-detail" element={<Dashboard />} />
-        </Routes>
-      </Layout>
+      <Provider store={store}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dex" element={<Dex />} />
+            <Route path="/pokemon-detail" element={<Dashboard />} />
+          </Routes>
+        </Layout>
+      </Provider>
     </BrowserRouter>
   );
 };
